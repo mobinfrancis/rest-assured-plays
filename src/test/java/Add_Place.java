591d1c9c517dcc721bdd4f969ee9808e59d0bdc2
Mobin Francis <mobin.francis@publicissapient.com>
2025-02-25 18:51:48 +0530

    initial commit add place

---
+++ src/test/java/Add_Place.java
@@ -0,0 +1,14 @@
+import io.restassured.RestAssured;
+import static
+
+import static io.restassured.RestAssured.given;.io.restassured.RestAssured.*;
+
+public class Add_Place
+{
+    public static void main(String[] args)
+    {
+        RestAssured.baseURI ="https://rahulshettyacademy.com";
+        given().
+    }
+}
+
